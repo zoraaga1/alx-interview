b3d099f1a3d010a59ca19be85f408f230e367630
zoraaga1 <fati.sousse12@gmail.com>
2024-06-07 01:24:57 +0100

    task0

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,6 +1,19 @@
 #!/usr/bin/python3
+"""
+Lockboxes module
+"""
+
+
 def canUnlockAll(boxes):
-    """Determines if all the boxes can be opened."""
+    """
+    Determines if all the boxes can be opened.
+
+    Parameters:
+    boxes (list of lists): List of boxes with keys to other boxes
+
+    Returns:
+    bool: True if all boxes can be opened, False otherwise
+    """
     n = len(boxes)
     unlocked = set()
     keys = [0]
