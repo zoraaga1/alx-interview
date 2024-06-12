2fbf9ac89ad1c1978879aad709e80d15cbb31a34
zoraaga1 <fati.sousse12@gmail.com>
2024-06-12 01:15:42 +0100

    task0

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""Minimum Operations module"""
+
+def minOperations(n: int) -> int:
+    """Minimum Operations function
+
+    Args:
+        n (int): number of operations
+
+    Returns:
+        int: number of operations
+    """
+
+    if n <= 1:
+        return 0
+
+    operations = 0
+    factor = 2
+    
+    while n > 1:
+        while n % factor == 0:
+            operations += factor
+            n //= factor
+        factor += 1
+    
+    return operations
