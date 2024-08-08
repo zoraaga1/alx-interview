179a2cf3b1056cb9606b013f076337b09a8dc530
zoraaga1 <fati.sousse12@gmail.com>
2024-08-08 14:08:17 +0100

    task0

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,6 +1,9 @@
+#!/usr/bin/python3
+
 def isWinner(x, nums):
     def sieve(n):
-        """ Helper function to compute prime numbers up to n using Sieve of Eratosthenes """
+        """ Helper function to compute prime numbers
+        up to n using Sieve of Eratosthenes """
         is_prime = [True] * (n + 1)
         p = 2
         while p * p <= n:
@@ -11,11 +14,12 @@ def isWinner(x, nums):
         return [p for p in range(2, n + 1) if is_prime[p]]
 
     def play_game(n):
-        """ Simulates a single game of choosing primes and removing primes and their multiples """
+        """ Simulates a single game of choosing primes
+        and removing primes and their multiples """  # noqa
         primes = sieve(n)
         prime_set = set(primes)
         turn = 0  # 0 for Maria's turn, 1 for Ben's turn
-        
+
         while primes:
             current_prime = primes.pop(0)
             if current_prime in prime_set:
@@ -23,13 +27,13 @@ def isWinner(x, nums):
                 prime_set -= multiples
                 primes = [p for p in primes if p in prime_set]
                 turn = 1 - turn
-        
+
         # If the last turn was Maria's (0), Ben wins (1), and vice versa
         return turn  # Return the winner: 1 for Ben, 0 for Maria
-    
+
     maria_wins = 0
     ben_wins = 0
-    
+
     for n in nums:
         if n < 2:
             ben_wins += 1
@@ -39,7 +43,7 @@ def isWinner(x, nums):
                 maria_wins += 1
             else:
                 ben_wins += 1
-    
+
     if maria_wins > ben_wins:
         return "Maria"
     elif ben_wins > maria_wins:
