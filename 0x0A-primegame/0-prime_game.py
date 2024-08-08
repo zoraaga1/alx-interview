110fe2e9e1f6ea1b55f178ffeba071b641b6351b
zoraaga1 <fati.sousse12@gmail.com>
2024-08-08 15:19:18 +0100

    task0

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,52 +1,21 @@
 #!/usr/bin/python3
+"""prime_game module"""
 
-def isWinner(x, nums):
-    def sieve(n):
-        """Helper function to compute prime
-        numbers up to n using Sieve of Eratosthenes."""
-        is_prime = [True] * (n + 1)
-        p = 2
-        while p * p <= n:
-            if is_prime[p]:
-                for i in range(p * p, n + 1, p):
-                    is_prime[i] = False
-            p += 1
-        return [p for p in range(2, n + 1) if is_prime[p]]
 
-    def play_game(n):
-        """Simulates a single game of choosing
-        primes and removing primes and their multiples."""
-        primes = sieve(n)
-        prime_set = set(primes)
-        turn = 0  # 0 for Maria's turn, 1 for Ben's turn
+def island_perimeter(grid):
+    """returns the perimeter of the island described in grid"""
 
-        while primes:
-            current_prime = primes.pop(0)
-            if current_prime in prime_set:
-                multiples = set(range(current_prime, n + 1, current_prime))
-                prime_set -= multiples
-                primes = [p for p in primes if p in prime_set]
-                turn = 1 - turn
+    width = len(grid[0])
+    height = len(grid)
+    edges = 0
+    size = 0
 
-        # If the last turn was Maria's (0), Ben wins (1), and vice versa
-        return turn  # Return the winner: 1 for Ben, 0 for Maria
-
-    maria_wins = 0
-    ben_wins = 0
-
-    for n in nums:
-        if n < 2:
-            ben_wins += 1
-        else:
-            winner = play_game(n)
-            if winner == 0:
-                maria_wins += 1
-            else:
-                ben_wins += 1
-
-    if maria_wins > ben_wins:
-        return "Maria"
-    elif ben_wins > maria_wins:
-        return "Ben"
-    else:
-        return None
+    for i in range(height):
+        for j in range(width):
+            if grid[i][j] == 1:
+                size += 1
+                if (j > 0 and grid[i][j - 1] == 1):
+                    edges += 1
+                if (i > 0 and grid[i - 1][j] == 1):
+                    edges += 1
+    return size * 4 - edges * 2
